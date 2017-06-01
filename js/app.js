84605b55d184b414cb78b542ace6e4b8ad13adcb
genarvaez <ge_naravez@hotmail.com>
2017-06-01 00:00:07 -0400

    iniciando la grilla

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+(function principal(){
+	var box = Array.from(document.getElementsByClassName("size"));
+	var modal = document.getElementById("box-work-modal");
+	box.forEach(function(e){
+		e.addEventListener("click",function(){
+			var bgModal = document.createElement("div");
+			bgModal.classList.add("background-modal");
+			bgModal.innerHTML = e.innerHTML;
+			modal.appendChild(bgModal);	
+			modal.classList.remove("hide");
+			var close = document.createElement("img");
+			close.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.setAttribute("id", "close");
+			bgModal.appendChild(close);
+			close.addEventListener("click", function(){
+				modal.classList.add("hide");
+			})
+		})
+	})
+})()
